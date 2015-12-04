97c7a43aae1b3586d30f65a3eb056d661e785827
Kinzi <webmarken@gmail.com>
2015-12-04 14:44:27 +0100

    Update stepper.js

--- directive/stepper.js
+++ directive/stepper.js
@@ -3,12 +3,12 @@ angular.module('stepper.directive', [])
 
 .directive('stepper', function () {
     return {
-		restrict: 'E',
-		replace: true,
-		templateUrl: 'stepper.html',
-		scope: {
+	restrict: 'E',
+	replace: true,
+	templateUrl: 'stepper.html',
+	scope: {
 	      model: "=model",
 	      placeholder: "@placeholder"
-	    }
+	}
     };
 });
